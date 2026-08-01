82a5aa1efe211a8362ecd76cf808fce146ca786c
Gaurav Kachawa <gauravkachawa16@gmail.com>
2026-08-01 23:52:29 +0530

    Added profile

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -19,15 +19,87 @@ router.post("/register", registerUser);
 
 router.post("/login", loginUser);
 
-router.get("/profile", protect, (req, res) => {
+router.get("/profile", protect, async (req, res) => {
 
-    res.json({
+    try{
 
-        message: "Protected Route Accessed",
+        const User = require("../models/User");
 
-        user: req.user
+        const user = await User.findById(req.user.id).select("-password");
 
-    });
+        if(!user){
+
+            return res.status(404).json({
+
+                message:"User not found"
+
+            });
+
+        }
+
+        res.json(user);
+
+    }
+
+    catch(error){
+
+        res.status(500).json({
+
+            message:error.message
+
+        });
+
+    }
+
+});
+
+router.put("/profile", protect, async (req,res)=>{
+
+    try{
+
+        const User = require("../models/User");
+
+        const user = await User.findById(req.user.id);
+
+        if(!user){
+
+            return res.status(404).json({
+
+                message:"User not found"
+
+            });
+
+        }
+
+        user.name=req.body.name || user.name;
+
+        user.skills=req.body.skills || user.skills;
+
+        user.portfolio=req.body.portfolio || user.portfolio;
+
+       
+
+        await user.save();
+
+        res.json({
+
+            message:"Profile Updated Successfully",
+
+            user
+
+        });
+
+    }
+
+    catch(error){
+
+        res.status(500).json({
+
+            message:error.message
+
+        });
+
+    }
 
 });
 
